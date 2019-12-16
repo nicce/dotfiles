76351b24a23baf3686973e447cfb39cd5eec58d8
Niclas Fredriksson <niclas.fredriksson1@ikea.com>
2019-12-16 21:52:08 +0100

    fix java identifier

--- scripts/install_sdk_man.sh
+++ scripts/install_sdk_man.sh
@@ -5,8 +5,9 @@ set -o pipefail
 curl -s "https://get.sdkman.io" | bash
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 11.0.5.j9
-sdk default java 11.0.5.j9
+
+sdk install java 11.0.5.j9-adpt
+sdk default java 11.0.5.j9-adpt
 
 java -version
 
