edc65d1bc9a5d8d6fb89bdd4ef531b105822437a
Niclas Fredriksson <niclas.fredriksson1@ikea.com>
2019-12-16 21:21:34 +0100

    debug

--- scripts/brew_installations.sh
+++ scripts/brew_installations.sh
@@ -13,13 +13,14 @@ brew tap homebrew/cask-versions
 brew cask install spotify
 brew cask install slack
 brew cask install visual-studio-code
+# docker will automaticlly be symlinked once started the first time
 brew cask install docker
-docker --version
 brew cask install java
-echo 'export JAVA_13_HOME=$(/usr/libexec/java_home -v13)' >> ~/.zshrc
-echo 'alias java13=export JAVA_HOME=$JAVA_13_HOME' >> ~/.zshrc
-echo '# default to Java 13' >> ~/.zshrc
-echo 'java13' >> ~/.zshrc
+echo "export JAVA_13_HOME=$(/usr/libexec/java_home -v13)" >> ~/.zshrc
+echo "alias java13='export JAVA_HOME=$JAVA_13_HOME'" >> ~/.zshrc
+echo "# default to Java 13" >> ~/.zshrc
+echo "java13" >> ~/.zshrc
+cat ~/.zshrc
 java -version
 
 # Clean up
