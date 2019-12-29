11d3abfe93ba8e556e59e550247ab7590f4d89d0
Niclas Fredriksson <niclas.fredriksson@tretton37.com>
2019-12-29 18:36:07 +0100

    possible fix for not overriding . files

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 scripts/install_homebrew.sh
 scripts/brew_installations.sh
 scripts/install_sdk_man.sh
-scripts/link_dotfiles.sh
 scripts/add_hosts.sh
 # Mac os tweaks
 scripts/clean_dock.sh
 scripts/menu_bar_config.sh
 # use oh-my-zsh as a custom zsh shell
 scripts/install_oh_my_zsh.sh
+# Link dotfiles last so we don't override them with any previous installations
+scripts/link_dotfiles.sh
