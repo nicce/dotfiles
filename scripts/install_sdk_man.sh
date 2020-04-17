a8f2f6e6795bd12bc7072bfc39e7c791305a44c9
Niclas Fredriksson <niclas.fredriksson@tretton37.com>
2020-04-17 20:31:38 +0200

    Bump java version and add wallpaper
    Note

--- scripts/install_sdk_man.sh
+++ scripts/install_sdk_man.sh
@@ -6,8 +6,8 @@ curl -s "https://get.sdkman.io" | bash
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java 11.0.5.j9-adpt
-sdk default java 11.0.5.j9-adpt
+sdk install java 14.0.0.j9-adpt
+sdk default java 14.0.0.j9-adpt
 
 java -version
 
